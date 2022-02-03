a3f4130fc3ddbf8e6a79a3b8bebf35c508b6ab19
Kenan <gurbanov.kanan02@gmail.com>
2022-02-03 21:20:14 +0400

    Deleted Form1

--- MathYouCan/Program.cs
+++ MathYouCan/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathYouCan.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace MathYouCan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
