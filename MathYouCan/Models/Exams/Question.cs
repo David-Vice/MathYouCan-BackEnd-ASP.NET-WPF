27729b07812eed3143fe1a0e4efedf4af2ba1123
Mame_ym86@itstep.edu.az <Mame_ym86@itstep.edu.az>
2022-06-22 12:40:21 +0400

    Models fixed

--- MathYouCan/Models/Exams/Question.cs
+++ MathYouCan/Models/Exams/Question.cs
@@ -11,6 +11,7 @@ namespace MathYouCan.Models.Exams
     {
         public int Id { get; set; }
         public QuestionType Type { get; set; }
+        public string QuestionContent { get; set; } = null;
         public string Text { get; set; } = null;
         public string PhotoName { get; set; } = null;
         public IEnumerable<QuestionAnswer> Answers { get; set; }
