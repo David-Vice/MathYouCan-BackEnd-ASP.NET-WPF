1a32731d0026175f2724d773f76349c8d8ce31a2
Javid <Medz_rp99@itstep.edu.az>
2022-07-10 18:42:08 +0400

    Added GetAllExamDetails Request

---
+++ ActAPI/Models/Dto/OfflineExamDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace ActAPI.Models.Dto
+{
+    public class OfflineExamDetails
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Date { get; set; }
+        public long? StartTime { get; set; }
+        public long? EndTime { get; set; }
+    }
+}
